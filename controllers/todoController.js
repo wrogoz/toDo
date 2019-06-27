e30458e9697d1fb563d69dd8f0ace04100766d34
Wojciech Rogóż <wrogoz@gmail.com>
2019-06-27 12:27:16 +0200

    workin on functionality - still doesn work

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,19 @@
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// basic data
+let data = [{item:"learn sth new"},{item:"learn more"},{item:"and more!"}];
+
+
 module.exports = (app)=>{
     app.get('/todo', function (req, res) {
-      
+        res.render('todo',{todo:data});
       });
 
-    app.post('/todo', function (req, res) {
-      
+    app.post('/todo', urlencodedParser, function (req, res) {
+      data.push(req.body);
+      res.json(data);
       });
 
     app.delete('/todo', function (req, res) {
